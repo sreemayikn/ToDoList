17e609f38150a9629d0080d6b175b5afa58d040c
Sreemayi Kamma Narisetty <sreemayikn@gmail.com>
2024-06-12 10:24:26 -0500

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Scholar on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
